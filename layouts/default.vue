cd12e09053e69ef90428ba84c46e18e38e281d3d
Ahmed <ahd.i.ktb@gmail.com>
2024-09-08 00:42:48 +0530

    added primevue and tailwind

---
+++ layouts/default.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="card">
+    <Toolbar style="border-radius: 3rem; padding: 1rem 1rem 1rem 1.5rem">
+      <template #start>
+        <Tabs value="home">
+          <TabList>
+            <NuxtLink v-for="tab in items" :key="tab.label" :to="tab.route">
+              <Tab v-if="tab.route" :value="tab.route">
+                <div class="h-full w-full flex items-center gap-2 text-inherit">
+                  <i :class="tab.icon" />
+                  <span>{{ tab.label }}</span>
+                </div>
+              </Tab>
+            </NuxtLink>
+          </TabList>
+        </Tabs>
+      </template>
+
+      <template #end>
+        <div class="flex items-center gap-2">
+          <Button label="Share" severity="contrast" size="small" />
+          <Avatar
+            image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+            style="width: 32px; height: 32px"
+          />
+        </div>
+      </template>
+    </Toolbar>
+    <slot></slot>
+  </div>
+  <RouterView />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Menubar from "primevue/menubar";
+import Tabs from "primevue/tabs";
+import TabList from "primevue/tablist";
+import Tab from "primevue/tab";
+import Toolbar from "primevue/toolbar";
+import TabPanels from "primevue/tabpanels";
+import TabPanel from "primevue/tabpanel";
+import Avatar from "primevue/avatar";
+
+const items = ref([
+  {
+    label: "Home",
+    icon: "pi pi-home",
+    route: "home",
+  },
+  {
+    label: "About",
+    icon: "pi pi-info-circle",
+    route: "about",
+  },
+  {
+    label: "Contact",
+    icon: "pi pi-envelope",
+    route: "contact",
+  },
+]);
+</script>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+        unstyled: true
+    },
+    importPT: { as: 'Aura', from: '~/presets/aura' }     //import and apply preset
+},
 })
